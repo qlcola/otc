c365f4f10bab7d97ffd67dec571399fd3a4961ff
luo <805055502@qq.com>
2018-02-06 18:32:48 +0800

    发布广告，创建订单

--- src/store/actions/index.js
+++
@@ -1,7 +0,0 @@
-import * as counter from './counter';
-import * as fetchApi from './fetchApi';
-
-export default {
-    ...counter,
-    ...fetchApi,
-};
--- src/store/mutations/index.js
+++
@@ -1,8 +0,0 @@
-
-import * as fetchApi from './fetchApi';
-import * as counter from './counter';
-
-export default {
-    ...fetchApi,
-    ...counter,
-};
